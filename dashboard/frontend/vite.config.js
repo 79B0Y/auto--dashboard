dd02bb46494927981fba5cf1c3d711400d8dca37
79B0Y <jackyao791104@gmail.com>
2025-05-25 21:49:28 +0800

    Update vite.config.js

--- dashboard/frontend/vite.config.js
+++ dashboard/frontend/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': 'http://localhost:3000',
+      '/ws': {
+        target: 'ws://localhost:3000',
+        ws: true
+      }
+    }
+  }
 });
